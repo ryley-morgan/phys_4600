0b3105d5feae8c607e4a32adb6b4137d4195d062
Ryley Morgan <r_morg@hotmail.com>
2019-09-08 21:46:41 -0700

    Create Hello-World code and compile it for task0

---
+++ task0/task0.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+File: task0.c
+Author: Ryley Morgan <ryley.morgan@email.kpu.ca>
+*/
+int main() {
+    /* main() prints "Hello, world!" to the terminal */ 
+    printf("Hello, World!");
+    return 0;
+}
