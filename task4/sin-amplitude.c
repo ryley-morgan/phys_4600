fcbed1100b3ff896401831c3397edfee68b8240d
Ryley Morgan <ryley-morgan@github.com>
2019-09-23 11:48:09 -0700

    [task4] Add sin-amplitude.c to calculate the absolute amplitude of a sine wave

---
+++ task4/sin-amplitude.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+File: sin-amplitude.c
+Author: ryley.morgan@email.kpu.ca <Ryley Morgan>
+
+Determines the absolute amplitude of a sinusoidal wave
+*/
+
+
