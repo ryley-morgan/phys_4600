0d0aec36ffe72ea77dbea43e95279939635cb1ef
Ryley Morgan <ryley-morgan@github.com>
2019-10-28 12:53:07 -0700

    [Task 5] Abstract functions to vi_tools

--- task5/src/main.c
+++ task5/src/main.c
@@ -3,127 +3,92 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "vi_tools.h"
 
 int main(int argc, char const *argv[])
 {
-    char resultBuffer[256];
-    char idnQuery[] = "*IDN?\n";
-    char dataSourceQuery[] = "DAT:SOU?\n";
-    char setChanOne[] = "DAT:SOU CH1\n";
-    char curveQuery[] = "CURV?\n";
-    char dataQuery[] = "DAT?\n";
-    char ch1ScaleQuery[] = "CH1:SCA?\n";
-    char encodeQuery[] = "DATA:ENCDG?\n";
-    char widthQuery[] = "DATA:WID?\n";
-    ViStatus status = VI_SUCCESS;
-    ViFindList resourceList;
-    ViUInt32 num_inst;
-    ViUInt32 resultCount=0;
-
-    ViSession defaultRM, scopeHandle;
-    ViChar description[VI_FIND_BUFLEN];
-    ViChar dataBuffer[2500];
-
-    int y;
-    float ch1DAC; // Digital to Analog conversion factor
-    int data_width;
+    int y, dataSource, dataWidth;
+    float ch1DAC, dataEncoding;
     float ch1Waveform[2500];
+    char idn[VI_FIND_BUFLEN];
 
+    //ViChar description[VI_FIND_BUFLEN];
+    ViChar dataBuffer[2500];
+    ViChar resultBuffer[VI_FIND_BUFLEN];
+
+    ViFindList resourceList;
+
+    ViUInt32 numInst;
+    ViUInt32 resultCount=0;
+    
+    ViStatus status = VI_SUCCESS;
+    ViSession defaultRM, scopeHandle, FGHandle;
+
+    // Open default remote connection
     status = viOpenDefaultRM(&defaultRM);
-
 
     if(status == VI_SUCCESS)
     {
-        //"USB[0-9]::0?*INSTR"
-        //"GPIB[0-9]*::?*INSTR"
-        //"USB::0x0699::0x0368::C026271::INSTR"
-        //status = viFindRsrc(defaultRM, "USB0::0x0699::0x0368::C026271::INSTR",&resourceList,&num_inst,description);
-        status = viFindRsrc(defaultRM, "USB[0-9]::0?*INSTR",&resourceList,&num_inst,description);
+        status = open_scope(&defaultRM,scopeHandle,&resourceList, &numInst);
         if(status == VI_SUCCESS)
-        {   // use "USB0::0x0699::0x0368::C026271::INSTR" instead of description below?
-            status = viOpen(defaultRM,description, VI_NULL,VI_NULL,&scopeHandle);
-            //ViStatus viGetAttribute(scopeHandle,VI_ATTR_TMO_VALUE, ViAttrState attrState);
-            //viGetAttribute(scopeHandle,(ViAttr)"VI_ATTR_TMO_VALUE");
-            status = viClear(scopeHandle); //Clear scope IO buffers
-            if(status == VI_SUCCESS)
+        {
+
+            // Get Scope IDN
+            get_idn(scopeHandle,resultBuffer,RESULT_BUFFER_SIZE); // From vi_tools.h
+            sscanf(resultBuffer,"%s",idn);
+            printf("Result Buffer = %s\n",idn);
+
+            // Set scopeHandle to use CH1
+            set_channel(scopeHandle, 1);
+            resultBuffer[0] = '\0'; // Clear result buffer
+            get_data_source(scopeHandle,resultBuffer,RESULT_BUFFER_SIZE); // Get data source
+            printf("Data Source = %s\n",resultBuffer); // Print current data source
+            sscanf(resultBuffer,"%i",&dataSource);
+
+            // Check data endoding
+            resultBuffer[0] = '\0';
+            get_data_encoding(scopeHandle,resultBuffer,RESULT_BUFFER_SIZE);
+            printf("Data Encoding = %s\n",resultBuffer);
+            sscanf(resultBuffer,"%e",&dataEncoding);
+
+            // Get channel data width
+            resultBuffer[0] = '\0';
+            get_data_width(scopeHandle,resultBuffer,RESULT_BUFFER_SIZE);
+            sscanf(resultBuffer,"%d",&dataWidth);
+            switch(dataWidth)      // Convert output from data width to 
             {
-                printf("Opened %s\n",description);
-                
-              /*viWrite(scopeHandle,idnQuery,(ViUInt32)strlen(idnQuery),&resultCount);
-                sleep(2);
-                viRead(scopeHandle,resultBuffer,256,&resultCount);
-                resultBuffer[resultCount] = 0;
-                */
-                // Query scope
-                
-
-                printf("Result Count = %d\n", resultCount);
-                printf("Result Buffer = %s\n", resultBuffer);
-
-                // Set scopeHandle to use channel one of the oscilloscope
-                viClear(scopeHandle);
-                viWrite(scopeHandle, setChanOne, (ViUInt32)strlen(setChanOne), &resultCount); sleep(2);
-                // Check what channel scopeHandle is using
-                viWrite(scopeHandle, dataSourceQuery, (ViUInt32)strlen(dataSourceQuery), &resultCount); sleep(2);
-                viRead(scopeHandle, resultBuffer, 256, &resultCount);
-                resultBuffer[resultCount] = 0;
-                printf("Data Source = %s\n",resultBuffer);
-                resultBuffer[0]='\0';
-
-                // Check data encoding
-                viWrite(scopeHandle, encodeQuery, (ViUInt32)strlen(encodeQuery), &resultCount); sleep(2);
-                viRead(scopeHandle, resultBuffer, 256, &resultCount);
-                resultBuffer[resultCount] = 0;
-                printf("Data Encoding = %s\n",resultBuffer);
-                resultBuffer[0]='\0';
-
-                // Check data width
-                viWrite(scopeHandle, widthQuery, (ViUInt32)strlen(widthQuery), &resultCount); sleep(2);
-                viRead(scopeHandle, resultBuffer, 256, &resultCount);
-                sscanf(resultBuffer,"%d",&data_width);
-                switch(data_width)
-                {
-                    case 1:
-                        data_width = 256;
-                        break;
-                    case 2:
-                        data_width = 65536;
-                        break;
-                }
-                resultBuffer[0]='\0';
-                printf("Data Width = %d\n", data_width);
-
-                //viWrite(scopeHandle, dataSourceQuery,(ViUInt32)strlen(dataSourceQuery),&resultCount);
-                //viRead(scopeHandle, resultBuffer, 256,&resultCount);
-
-                // Get voltage scale data from channel 1
-                viClear(scopeHandle);
-                viWrite(scopeHandle, ch1ScaleQuery, (ViUInt32)strlen(ch1ScaleQuery), &resultCount); sleep(2);
-                viRead(scopeHandle,resultBuffer,256,&resultCount);
-                sscanf(resultBuffer,"%f", &ch1DAC);
-                printf("CH1 Voltage Scale: %f (Volts/Div)\n",ch1DAC);
-                ch1DAC = ch1DAC*8/data_width; //Scale for Digital-to-Analog conversion
-                printf("CH1 Digital to Analog conversion factor: %f (Volts/adc_step)\n", ch1DAC);
-                resultBuffer[0]='\0';
-
-                // Get scope curve
-                get_curve(scopeHandle, dataBuffer, 2500);
-                
-                //printf("[WARN] - READ STATUS: %d",status);
-                for (int i = 0; i < 2500; ++i)
-                {
-                    y = dataBuffer[i];
-                    ch1Waveform[i] = y*ch1DAC;
-                    printf("Raw = %x,\tRead = %d,\t Value = %f\n",y,y,ch1Waveform[i]);
-                }
+                case 1:
+                    dataWidth = 256/2;
+                    break;
+                case 2:
+                    dataWidth = 65536/2;
+                    break;
             }
-            else
-                printf("Failed to open %s",description);
+            printf("Data Width = %d\n", dataWidth);
+
+            // Get channel 1 voltage scale
+            resultBuffer[0] = '\0';
+            get_voltage(scopeHandle,1,resultBuffer,RESULT_BUFFER_SIZE);
+            sscanf(resultBuffer,"%f", &ch1DAC);
+            printf("CH1 Voltage Scale: %f (Volts/Div)\n",ch1DAC);
+            ch1DAC = ch1DAC*10/dataWidth; //Scale for Digital-to-Analog conversion with 10 full divisions (2 are missing from screen)
+            printf("CH1 Digital to Analog conversion factor: %f (Volts/adc_step)\n", ch1DAC);
+
+            // Get scope curve
+            get_curve(scopeHandle, dataBuffer, 2500);
+        
+            // Convert data using digital to analog conversion factor: ch1DAC
+            for (int i = 0; i < 2500; ++i)
+            {
+                y = dataBuffer[i];
+                ch1Waveform[i] = y*ch1DAC;
+                printf("Raw = %x,\tRead = %d,\t Value = %f\n",y,y,ch1Waveform[i]);
+            }
         }
-        else
-            printf("Couldn't find any instruments");
+
     }
     else
         printf("Failed to open defaultRM");
     return 0;
 }
+
--- task5/src/vi_tools.c
+++ task5/src/vi_tools.c
@@ -7,64 +7,138 @@
 // Define scope commands
 #define GET_CURV "CURV?\n"
 #define GET_DATA "DAT?\n"
-#define GET_DATA_SRC "DAT:SOU?\n"
 #define GET_DATA_ENC "DATA:ENCDG?\n"
 #define GET_DATA_WID "DATA:WID?\n"
-#define GET_VOLT "CH%i:SCA?\n"
+#define GET_DATA_SRC "DAT:SOU?\n"
 #define GET_IDN "*IDN?\n"
+#define GET_VOLT "CH%i:SCA?\n"
 #define SET_CHX "DAT:SOU CH%i\n"
 #define SET_VOLT "CH%i:SCA %E\n"
-
 #define RESULT_BUFFER_SIZE 256
+#define DEFAULT_COMMAND_SIZE 36
 
 ViChar resultBuffer[RESULT_BUFFER_SIZE];
 
+// Open scope
+ViStatus open_scope(ViSession * defaultRM, ViSession handle, ViFindList * resourceList, ViUInt32 * numInst)
+{
+    ViChar description[VI_FIND_BUFLEN];
+    ViStatus status;
+    // Read scope info to populate description and resource list
+    status = viFindRsrc(*defaultRM, "USB[0-9]::0x0699?*INSTR",resourceList,numInst,description);  // Techtronics Scopes (Vendor id: 0x0699)
+    if(status == VI_SUCCESS)
+    {
+        status = viOpen(*defaultRM,description, VI_NULL,VI_NULL,&handle);
+        // Confirm scope opened correctly
+        if(status == VI_SUCCESS)
+        {
+            printf("Opened %s\n",description);
+            viClear(handle); //Clear scope IO buffers
+        }
+        else
+            printf("Failed to open %s",description);
+    }
+    else
+        printf("Couldn't find any instruments");
+    return status;
+}
 
-ViStatus get_curve(ViSession handle, int channel, char * dataBuffer, int npoints)
+// Get functions
+ViStatus get_curve(ViSession handle, ViChar * dataBuffer, int npoints)
 {
     ViUInt32 resultCount;
     ViStatus status;
+    viClear(handle);
     viWrite(handle, GET_CURV, (ViUInt32)strlen(GET_CURV), &resultCount); //Specific to our oscilloscope
-    sleep(10);
+    sleep(2);
     status = viRead(handle, dataBuffer, npoints, &resultCount);
     return status;
 }
 
-ViStatus get_voltage(ViSession handle, int channel, char* resultBuffer, int bufferSize)
+ViStatus get_data(ViSession handle, ViChar * resultBuffer, int bufferSize)
 {
     ViUInt32 resultCount;
     ViStatus status;
-    char * command[36];
-    sprintf(command, GET_VOLT, channel);
-    iWrite(handle, command, (ViUInt32)strlen(command), &resultCount); //Specific to our oscilloscope
-    sleep(10);
-    status = viRead(handle, dataBuffer, bufferSize, &resultCount);
+    viClear(handle);
+    viWrite(handle, GET_DATA, (ViUInt32)strlen(GET_DATA), &resultCount); //Specific to our oscilloscope
+    sleep(2);
+    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
     return status;
 }
 
+ViStatus get_data_encoding(ViSession handle, ViChar * resultBuffer, int bufferSize)
+{
+    ViUInt32 resultCount;
+    ViStatus status;
+    viClear(handle);
+    viWrite(handle, GET_DATA_ENC, (ViUInt32)strlen(GET_DATA_ENC), &resultCount); //Specific to our oscilloscope
+    sleep(2);
+    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    return status;
+}
+
+ViStatus get_data_width(ViSession handle, ViChar * resultBuffer, int bufferSize)
+{
+    ViUInt32 resultCount;
+    ViStatus status;
+    viClear(handle);
+    viWrite(handle, GET_DATA_WID, (ViUInt32)strlen(GET_DATA_WID), &resultCount); //Specific to our oscilloscope
+    sleep(2);
+    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    return status;
+}
+
+ViStatus get_data_source(ViSession handle, ViChar * resultBuffer, int bufferSize)
+{
+    ViUInt32 resultCount;
+    ViStatus status;
+    viClear(handle);
+    viWrite(handle, GET_DATA_SRC, (ViUInt32)strlen(GET_DATA_SRC), &resultCount); //Specific to our oscilloscope
+    sleep(2);
+    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    return status;
+}
+
+ViStatus get_idn(ViSession handle, ViChar * resultBuffer, int bufferSize)
+{
+    ViUInt32 resultCount;
+    ViStatus status;
+    viClear(handle);
+    viWrite(handle, GET_IDN, (ViUInt32)strlen(GET_IDN), &resultCount); //Specific to our oscilloscope
+    sleep(2);
+    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    return status;
+}
+
+ViStatus get_voltage(ViSession handle, int channel, ViChar * resultBuffer, int bufferSize)
+{
+    ViUInt32 resultCount;
+    ViStatus status;
+    char command[DEFAULT_COMMAND_SIZE];
+    sprintf(command, GET_VOLT, channel);
+    viClear(handle);
+    viWrite(handle, command, (ViUInt32)strlen(command), &resultCount); //Specific to our oscilloscope
+    sleep(2);
+    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    return status;
+}
+
+
+// Set Functions
 void set_channel(ViSession handle, int channel)
 {
     ViUInt32 resultCount;
-    char * command[36];
+    char command[DEFAULT_COMMAND_SIZE];
     sprintf(command, SET_CHX, channel);
+    viClear(handle);
     viWrite(handle, command, (ViUInt32)strlen(command), &resultCount);
 }
 
 void set_voltage(ViSession handle, int channel, float volts)
 {
     ViUInt32 resultCount;
-    char * command[36];
-    sprintf(command, GET_VOLT_CH1, channel, volts);
+    char command[DEFAULT_COMMAND_SIZE];
+    sprintf(command, SET_VOLT, channel, volts);
+    viClear(handle);
     viWrite(handle, command, (ViUInt32)strlen(command), &resultCount);
-}
-
-
-
-ViStatus init_scope(ViSession handle)
-{
-    // Set channel
-    // Set voltage
-    // Set width
-
-    return status;
 }
