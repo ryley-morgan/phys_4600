2fd7b62f96861c68f24b6cd3258129882bd34f32
Ryley Morgan <ryley-morgan@github.com>
2019-10-28 14:51:11 -0700

    Debugging output issues

--- task5/src/vi_tools.c
+++ task5/src/vi_tools.c
@@ -13,14 +13,16 @@
 #define GET_IDN "*IDN?\n"
 #define GET_VOLT "CH%i:SCA?\n"
 #define SET_CHX "DAT:SOU CH%i\n"
+#define SET_DATA_START "DAT:STAR 1"
+#define SET_DATA_STOP "DAT:STOP 2500"
 #define SET_VOLT "CH%i:SCA %E\n"
+#define DATA_BUFFER_SIZE 2500
 #define RESULT_BUFFER_SIZE 256
 #define DEFAULT_COMMAND_SIZE 36
 
-ViChar resultBuffer[RESULT_BUFFER_SIZE];
 
 // Open scope
-ViStatus open_scope(ViSession * defaultRM, ViSession handle, ViFindList * resourceList, ViUInt32 * numInst)
+ViStatus open_scope(ViSession * defaultRM, ViSession * handle, ViFindList * resourceList, ViUInt32 * numInst)
 {
     ViChar description[VI_FIND_BUFLEN];
     ViStatus status;
@@ -28,12 +30,12 @@ ViStatus open_scope(ViSession * defaultRM, ViSession handle, ViFindList * resour
     status = viFindRsrc(*defaultRM, "USB[0-9]::0x0699?*INSTR",resourceList,numInst,description);  // Techtronics Scopes (Vendor id: 0x0699)
     if(status == VI_SUCCESS)
     {
-        status = viOpen(*defaultRM,description, VI_NULL,VI_NULL,&handle);
+        status = viOpen(*defaultRM,description, VI_NULL,VI_NULL,handle);
         // Confirm scope opened correctly
         if(status == VI_SUCCESS)
         {
             printf("Opened %s\n",description);
-            viClear(handle); //Clear scope IO buffers
+            viClear(*handle); //Clear scope IO buffers
         }
         else
             printf("Failed to open %s",description);
@@ -55,62 +57,63 @@ ViStatus get_curve(ViSession handle, ViChar * dataBuffer, int npoints)
     return status;
 }
 
-ViStatus get_data(ViSession handle, ViChar * resultBuffer, int bufferSize)
+ViStatus get_data(ViSession handle, ViChar * resultBuf, int bufferSize)
 {
     ViUInt32 resultCount;
     ViStatus status;
     viClear(handle);
     viWrite(handle, GET_DATA, (ViUInt32)strlen(GET_DATA), &resultCount); //Specific to our oscilloscope
     sleep(2);
-    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    status = viRead(handle, resultBuf, bufferSize, &resultCount);
     return status;
 }
 
-ViStatus get_data_encoding(ViSession handle, ViChar * resultBuffer, int bufferSize)
+ViStatus get_data_encoding(ViSession handle, ViChar * resultBuf, int bufferSize)
 {
     ViUInt32 resultCount;
     ViStatus status;
     viClear(handle);
     viWrite(handle, GET_DATA_ENC, (ViUInt32)strlen(GET_DATA_ENC), &resultCount); //Specific to our oscilloscope
     sleep(2);
-    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    status = viRead(handle, resultBuf, bufferSize, &resultCount);
     return status;
 }
 
-ViStatus get_data_width(ViSession handle, ViChar * resultBuffer, int bufferSize)
+ViStatus get_data_width(ViSession handle, ViChar * resultBuf, int bufferSize)
 {
     ViUInt32 resultCount;
     ViStatus status;
     viClear(handle);
     viWrite(handle, GET_DATA_WID, (ViUInt32)strlen(GET_DATA_WID), &resultCount); //Specific to our oscilloscope
     sleep(2);
-    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    status = viRead(handle, resultBuf, bufferSize, &resultCount);
     return status;
 }
 
-ViStatus get_data_source(ViSession handle, ViChar * resultBuffer, int bufferSize)
+
+ViStatus get_data_source(ViSession handle, ViChar * resultBuf, int bufferSize)
 {
     ViUInt32 resultCount;
     ViStatus status;
     viClear(handle);
     viWrite(handle, GET_DATA_SRC, (ViUInt32)strlen(GET_DATA_SRC), &resultCount); //Specific to our oscilloscope
     sleep(2);
-    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    status = viRead(handle, resultBuf, bufferSize, &resultCount);
     return status;
 }
 
-ViStatus get_idn(ViSession handle, ViChar * resultBuffer, int bufferSize)
+ViStatus get_idn(ViSession handle, ViChar * resultBuf, int bufferSize)
 {
     ViUInt32 resultCount;
     ViStatus status;
     viClear(handle);
     viWrite(handle, GET_IDN, (ViUInt32)strlen(GET_IDN), &resultCount); //Specific to our oscilloscope
     sleep(2);
-    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    status = viRead(handle, resultBuf, bufferSize, &resultCount);
     return status;
 }
 
-ViStatus get_voltage(ViSession handle, int channel, ViChar * resultBuffer, int bufferSize)
+ViStatus get_voltage(ViSession handle, int channel, ViChar * resultBuf, int bufferSize)
 {
     ViUInt32 resultCount;
     ViStatus status;
@@ -119,7 +122,7 @@ ViStatus get_voltage(ViSession handle, int channel, ViChar * resultBuffer, int b
     viClear(handle);
     viWrite(handle, command, (ViUInt32)strlen(command), &resultCount); //Specific to our oscilloscope
     sleep(2);
-    status = viRead(handle, resultBuffer, bufferSize, &resultCount);
+    status = viRead(handle, resultBuf, bufferSize, &resultCount);
     return status;
 }
 
@@ -132,6 +135,20 @@ void set_channel(ViSession handle, int channel)
     sprintf(command, SET_CHX, channel);
     viClear(handle);
     viWrite(handle, command, (ViUInt32)strlen(command), &resultCount);
+}
+
+void set_data_start(ViSession handle)
+{
+    ViUInt32 resultCount;
+    viClear(handle);
+    viWrite(handle, SET_DATA_START, (ViUInt32)strlen(SET_DATA_START), &resultCount); //Specific to our oscilloscope
+}
+
+void set_data_stop(ViSession handle)
+{
+    ViUInt32 resultCount;
+    viClear(handle);
+    viWrite(handle, SET_DATA_STOP, (ViUInt32)strlen(SET_DATA_START), &resultCount); //Specific to our oscilloscope
 }
 
 void set_voltage(ViSession handle, int channel, float volts)
