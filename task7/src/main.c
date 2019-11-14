e40cbafd3c82ad1d89910f51805ae5ff8542d245
Ryley Morgan <ryley-morgan@github.com>
2019-11-14 10:38:06 -0800

    [Task7] Blinking LED

--- task7/src/main.c
+++ task7/src/main.c
@@ -4,12 +4,13 @@
 
 uint8_t keep_going = 1;
 
+/*
 void loop()
 {
     //PORTD |= 2; // Bitwise OR operation to change pin PD1 to high without affecting the rest of PORTD
     PORTD |= (1 << PD1);    // Bitwise OR operation using bit-shifting via macros specified in the header files
     _delay_ms(50);
-    PORTD &= ~(1 << PD1);
+    PORTD &= ~(1 << PD1);  // Bitwise OR operation to unset this pin
     _delay_ms(50);
 }
 
@@ -23,5 +24,27 @@ int main()
     setup();
     while(keep_going) loop();
  
+    return 0;
+}
+*/
+
+//Adding interupt functions - Called everytime Timer0 overflows
+ISR(TIMER0_OVF_vect)
+{
+    PORTD ^= (1<<PD1);   //Bitwise xor
+}
+
+void setup()
+{
+    // PortD-Pin1 setup
+    DDRD |= (1<<PD1);   //set DATADIRECTIOND to say pin d is an output pin
+    PORTD &= ~(1<<PD1); // start pin as 0
+
+
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
     return 0;
 }
