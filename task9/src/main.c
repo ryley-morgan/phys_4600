43085ae8d367169957cfe18220cd325eb2935975
Ryley Morgan <ryley-morgan@github.com>
2019-11-21 11:05:27 -0800

    [Task9] finalized task

--- task9/src/main.c
+++ task9/src/main.c
@@ -32,12 +32,12 @@ int main()
 ISR(TIMER0_OVF_vect)
 {
     //PORTD ^= (1<<PD1);   // Bitwise XOR (flip state)
-    PORTD &= ~(1 << PD1);
+    PORTD &= ~(1 << PD6);
 }
 
 ISR(TIMER0_COMPA_vect)
 {
-    PORTD |= (1 << PD1);
+    PORTD |= (1 << PD6);
 }
 
 void loop()
@@ -48,15 +48,28 @@ void loop()
 void setup()
 {
     // PortD-Pin1 setup
-    DDRD |= (1<<PD1);   //set DATADIRECTIOND to say pin d is an output pin
-    PORTD &= ~(1<<PD1); // start pin as 0
+    //DDRD |= (1<<PD1);   //set DATADIRECTIOND to say pin d is an output pin
+    //PORTD &= ~(1<<PD1); // start pin as 0
+
+    //TCCR0B = 0x01;   //TimerControlRegister0 set to operate in normal counting mode with no scaling (see data sheet)
+
+// PortD-Pin6 setup (COM0)
+    DDRD |= (1<<PD6) | (1<<PD5);   //set DATADIRECTIOND to say pin d is an output pin
+    
+    PORTD &= ~(1<<PD6); // start pin as 0
+
+    //TCCR0B = 0x01;   //TimerControlRegister0 set to operate in normal counting mode with no scaling (see data sheet)
 
     // Timer setup
-    TCCR0B = 0x01;   //TimerControlRegister0 set to operate in normal counting mode with no scaling (see data sheet)
+    TCCR0A |= (1<<COM0A0) | (1<<COM0A1) | (1<<COM0B0) | (1<<COM0B1);
+    TCCR0A |= (1<<WGM01) | (1<<WGM00);
+    TCCR0B |= (1<<CS00);
+    
     //TIMSK0 = 1;
-    TIMSK0 |= (1<<OCIE0A)|(1<<TOIE0);
+    //TIMSK0 |= (1<<OCIE0A)|(1<<TOIE0);
 
-    OCR0A = 200; // Number to compare
+    OCR0A = 100; // Number to compare
+    OCR0B = 50;
 
     sei();  // Start looking for timer interupts
 
